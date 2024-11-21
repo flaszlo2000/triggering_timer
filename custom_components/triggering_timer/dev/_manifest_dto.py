007273444070857296cbea589f3919e56567c28e
flaszlo2000 <f.laszlo2000@gmail.com>
2024-11-21 19:09:03 +0100

    updated docs, added hacs related stuff

--- custom_components/triggering_timer/dev/_manifest_dto.py
+++ custom_components/triggering_timer/dev/_manifest_dto.py
@@ -10,5 +10,8 @@ class ManifestDTO:
     name: str = field(default = Config.NAME.value)
     version: str = field(default = __version__)
 
+    codeowners: List[str] = field(default = ["@flaszlo2000"])
+    documentation: str = field(default = "https://github.com/flaszlo2000/triggering_timer")
+
     dependencies: List[str] = field(default_factory = list)
     requirements: List[str] = field(default_factory = list)
